be251d1a3a87cc9ca67b547e163c05bcba4d6119
charleshe100 <145732117+charleshe100@users.noreply.github.com>
2023-12-29 14:57:02 +0800

    建立會員註冊功能

--- front/reg.php
+++ front/reg.php
@@ -5,25 +5,54 @@
     <table>
         <tr>
             <td class="clo">Step1：帳號</td>
-            <td><input type="text" name="acc" id="acc"></td>
+            <td><input type="text" name="acc" id="acc" max="12"></td>
         </tr>
         <tr>
             <td class="clo">Step2：密碼</td>
-            <td><input type="password" name="pw" id="pw"></td>
+            <td><input type="password" name="pw" id="pw" max="12"></td>
         </tr>
         <tr>
             <td class="clo">Step3：再次確認密碼</td>
-            <td><input type="password" name="pw2" id="pw2"></td>
+            <td><input type="password" name="pw2" id="pw2" max="12"></td>
         </tr>
         <tr>
             <td class="clo">Step4：信箱（忘記密碼時使用）</td>
-            <td><input type="email" name="email" id="email"></td>
+            <td><input type="text" name="email" id="email"></td>
         </tr>
         <tr>
-            <td><input type="submit" value="註冊">
+            <td><input type="button" value="註冊" onclick="reg()">
+            <!-- type的submit只會送到form，但這裡不用form, 所以改成button -->
             <input type="reset" value="清除"></td>
         </tr>
-
     </table>
 </fieldset>
 </div>
+
+<script>
+function reg(){
+    let user={
+        acc:$("#acc").val(),
+        pw:$("#pw").val(),
+        pw2:$("#pw2").val(),
+        email:$("#email").val(),
+    }
+    if(user.acc!='' && user.pw!='' && user.pw2!='' && user.email!=''){
+        if(user.pw == user.pw2){
+            $.post("./api/chk_acc.php",{acc:user.acc},(res)=>{
+                // console.log(res)
+                if(parseInt(res)==1){
+                    alert("帳號重覆")
+                }else{
+                    $.post("./api/reg.php",user,(res)=>{
+                        alert('註冊完成，歡迎加入')
+                    })
+                }
+            })
+        }else{
+            alert("密碼錯誤")
+        }
+    }else{
+        alert("不可空白")
+    }
+}
+</script>
